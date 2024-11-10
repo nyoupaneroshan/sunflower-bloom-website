cb46b4fb32fa989b09033d30e420cef5a3580a2c
Roshan Nyoupane <nyoupane@Roshans-MacBook-Pro-2.local>
2024-11-10 21:13:24 +0545

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(3010, () => {
+    console.log('App running on http://localhost:3010');
+});
+
